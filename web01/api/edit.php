723f7a433e8c086f2b45885c252f2455597b3b43
lin1130011 <lin1130011@gmail.com>
2024-08-13 15:22:19 +0800

    第一題練習

--- web01/api/edit.php
+++ web01/api/edit.php
@@ -8,12 +8,27 @@ $db = new DB($table);
 foreach ($_POST['id'] as $key => $id) {
     if (!empty($_POST['del']) && in_array($id, $_POST['del'])) {
         $db->del($id);
+    } else {
+        $row = $db->find($id);
+        switch ($table) {
+            case 'title':
+                $row['sh'] = (isset($_POST['sh']) && $_POST['sh'] == $id) ? 1 : 0;
+                $row['text'] = $_POST['text'][$key];
+                break;
+
+            case 'ad':
+                $row['sh'] = (isset($_POST['sh']) && in_array($id, $_POST['sh'])) ? 1 : 0;
+                $row['text'] = $_POST['text'][$key];
+                break;
+            case 'mvim':
+                $row['sh'] = (isset($_POST['sh']) && in_array($id, $_POST['sh'])) ? 1 : 0;
+                break;
+        }
+
+
+        $db->save($row);
     }
-    $row = $db->find($id);
-
-    $row['sh'] = (isset($_POST['sh']) && $_POST['sh'] == $id) ? 1 : 0;
-    $row['text'] = $_POST['text'][$key];
-
-    $db->save($row);
 }
+to("../admin.php?do=$table");
+
 ?>
